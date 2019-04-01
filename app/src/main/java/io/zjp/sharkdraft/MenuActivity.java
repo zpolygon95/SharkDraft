5b2c3ef23ce253d74622cad8d6d9581dcf984a9a
Zachary Polikarpus (empoleon) <zpolygon95@gmail.com>
2019-03-31 20:05:22 -0400

    Add Database Test Stuff

--- app/src/main/java/io/zjp/sharkdraft/MenuActivity.java
+++ app/src/main/java/io/zjp/sharkdraft/MenuActivity.java
@@ -1,16 +1,25 @@
 package io.zjp.sharkdraft;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MenuActivity extends AppCompatActivity {
+
+    TextView dbInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,8 @@ public class MenuActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        dbInfo = findViewById(R.id.txtDBResults);
     }
 
     public void testAuth(View view) {
@@ -31,6 +42,24 @@ public class MenuActivity extends AppCompatActivity {
         FirebaseAuth auth = FirebaseAuth.getInstance();
         FirebaseUser user = auth.getCurrentUser();
         Toast.makeText(this, user.getUid(), Toast.LENGTH_SHORT).show();
+    }
+
+    public void testDB(View view) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("leagues").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    String out = "List of Leagues:\n";
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        out += document.getId() + ": " + document.getString("Name") + "\n";
+                    }
+                    dbInfo.setText(out);
+                } else {
+                    Toast.makeText(MenuActivity.this, "Error =(", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     public void navigateManualDraft(View view) {
